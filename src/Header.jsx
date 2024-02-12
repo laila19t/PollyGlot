df3fa041e9c9985d128cbf600379b1552ab614f3
laila19t <127900249+laila19t@users.noreply.github.com>
2024-02-13 01:29:30 +0400

    added header

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <div className='header'>
+        <h2>PollyGlot</h2>
+        <h3>Perfect Translation Every Time</h3>
+       </div>
+    )
+}
