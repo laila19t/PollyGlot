f2fa2e9ce4e94afd18574c21d33aa092e94f190c
laila19t <127900249+laila19t@users.noreply.github.com>
2024-02-13 01:47:34 +0400

    header css changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './Header'
+import Body from './Body'
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className='outer-container'>
       <Header/>
+      <Body/>
     </div>
   )
 }
