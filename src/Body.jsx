8713078b988d9f8c66b643f11f7d4de3a23df031
laila19t <127900249+laila19t@users.noreply.github.com>
2024-02-14 22:28:08 +0400

    added new font

--- src/Body.jsx
+++ src/Body.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Body(){
     return(
-        <>
-        h
-        </>
+        <div>
+            <h3>Text to translate</h3>
+        </div>
     )
 }
